551c9b706290820e75c5d627fb4b3ad083bef744
msakal <m_sakalauskas@hotmail.com>
2022-08-11 11:59:23 -0300

    finalizando

--- dedicada/app.js
+++ dedicada/app.js
@@ -1,7 +1,14 @@
 import {artistas} from './modulo.mjs';
 
 let tamanho = artistas.length;
-
-for ( let i = 0; i < tamanho; i++) {
+let i;
+for ( i = 0; i < tamanho; i++) {
     console.log(`${i} -  ${artistas[i]}`)
+}
+
+console.log('-----------------------');
+i = 0;
+while (i < tamanho) {
+    console.log(`${i} -  ${artistas[i]}`);
+    i ++;
 }
