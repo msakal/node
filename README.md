956513e0dbe8790ca1c6b6679eb372734d85544a
msakal <m_sakalauskas@hotmail.com>
2022-08-16 11:54:28 -0300

    finalizando aula nodejs

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
 # node
  aulas de nodejs
+
+ # criar arquivo
+ type nul > index.js
+
+# criar diretório
+ mkdir exemplo5
+
+ # criando pacote npm
+ https://www.npmjs.com/package/chalk
+ - executar na linha comando (npm install chalk)
---
+++ exemplo5/index.js
@@ -0,0 +1,19 @@
+import chalk from 'chalk';
+
+console.log(chalk.blue('Trabalhando com NPM'));
+console.log(chalk.red('--------------------'));
+console.log();
+
+let aluno = "Marcello";
+let idade = 48
+
+if ( idade >= 18 ) {
+    console.log(`${aluno} ${chalk.blue.bgYellow.bold(`é maior de idade!`)}`);
+} else { 
+    console.log(`${aluno} ${chalk.blue.bgRed.bold("é menor de idade!")}`);
+}
+console.log();
+console.log(chalk.magenta('--------------------'));
+
+// Obs: para instalar o chalk, utilizamos o comando 'npm install chalk' (readme.md)
+console.log();
